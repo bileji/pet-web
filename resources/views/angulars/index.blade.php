206ddaae2c7ea86c3a8ce6ccf46dc3d3a9b16d4a
shuchao <shuchao@medlinker.net>
2016-05-05 16:05:42 +0800

    ...

--- resources/views/angulars/index.blade.php
+++ resources/views/angulars/index.blade.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{URL::asset('assets/styles/styles.css')}}">
 
-    <script src="{{URL::asset('node_modules/es6-shim/es6-shim.min.js')}}"></script>
+    <script src="{{asset('node_modules/es6-shim/es6-shim.min.js')}}"></script>
 
-    <script src="{{URL::asset('node_modules/zone.js/dist/zone.js')}}"></script>
-    <script src="{{URL::asset('node_modules/reflect-metadata/Reflect.js')}}"></script>
-    <script src="{{URL::asset('node_modules/systemjs/dist/system.src.js')}}"></script>
+    <script src="{{asset('node_modules/zone.js/dist/zone.js')}}"></script>
+    <script src="{{asset('node_modules/reflect-metadata/Reflect.js')}}"></script>
+    <script src="{{asset('node_modules/systemjs/dist/system.src.js')}}"></script>
 
     <!-- 2. Configure SystemJS -->
-    <script src="{{URL::asset('public/scripts/systemjs.config.js')}}"></script>
+    <script src="{{asset('scripts/systemjs.config.js')}}"></script>
     <script>
         System.import('app').catch(function (err) {
             console.error(err);
