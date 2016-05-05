09edfe760518156040d65e0a403524c65cc0a5a1
shuchao <shuchao@medlinker.net>
2016-05-05 16:09:07 +0800

    ..

--- public/scripts/systemjs.config.js
+++ public/scripts/systemjs.config.js
@@ -10,9 +10,9 @@
 
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
-        'app': {main: 'main.js', defaultExtension: 'js'},
+        'app': {main: 'main.ts', defaultExtension: 'ts'},
         'rxjs': {defaultExtension: 'js'},
-        'angular2-in-memory-web-api': {defaultExtension: 'js'},
+        'angular2-in-memory-web-api': {defaultExtension: 'js'}
     };
 
     var packageNames = [
