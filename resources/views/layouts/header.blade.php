c50d53b0bbbcd8f2b1a9a6327760f197a1303ae9
shuchao <645904877@qq.com>
2016-05-14 14:42:13 +0800

    test

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,8 +2,19 @@
     <div class="nav-pull"></div>
     <div class="nav-banner">
         <ul class="nav-sign">
-            <li>sign in</li>
-            <li>sign out</li>
+            <li>
+                <div>我的菜谱 </div>
+            </li>
+            <li>
+                <div>商家支持 </div>
+            </li>
+            <li class="separator"></li>
+            <li>
+                <div>登录 </div>
+            </li>
+            <li>
+                <div>注册</div>
+            </li>
         </ul>
     </div>
 </header>
