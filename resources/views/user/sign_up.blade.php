016e18f071dac0dbd3054a788e8259d2c62eac32
shuchao <645904877@qq.com>
2016-05-16 23:02:47 +0800

    progress

--- resources/views/user/sign_up.blade.php
+++ resources/views/user/sign_up.blade.php
@@ -68,34 +68,39 @@
                         <input type="text" class="form-control" required ng-model="placeholder.phone">
                     </div>
 
-                    <div id="div_geetest_lib">
-                        <div id="captcha"></div>
-                        <script src="http://api.geetest.com/get.php?gt=421b84eeaee7b2aed4c0ec5706d8b571" async></script>
-                        <script>
-                            var handler = function (captchaObj) {
-                                // 将验证码加到id为captcha的元素里
-                                captchaObj.appendTo("#captcha");
-                            };
-                            $.ajax({
-                                // 获取id，challenge，success（是否启用failback）
-                                url: "{{asset("verify/captcha?rand=")}}" + Math.round(Math.random()*100),
-                                type: "get",
-                                dataType: "json",
-                                success: function (data) {
-                                    // 使用initGeetest接口
-                                    // 参数1：配置参数，与创建Geetest实例时接受的参数一致
-                                    // 参数2：回调，回调的第一个参数验证码对象，之后可以使用它做appendTo之类的事件
-                                    initGeetest({
-                                        gt: data.gt,
-                                        challenge: data.challenge,
-                                        product: "float", // 产品形式
-                                        offline: !data.success
-                                    }, handler);
-                                }
-                            });
-                        </script>
+                    <div>
+                        <div id="div_geetest_lib">
+                            <div id="captcha"></div>
+                            <script src="http://api.geetest.com/get.php?gt=421b84eeaee7b2aed4c0ec5706d8b571" async></script>
+                            <script>
+                                var handler = function (captchaObj) {
+                                    // 将验证码加到id为captcha的元素里
+                                    captchaObj.appendTo("#captcha");
+                                };
+                                $.ajax({
+                                    // 获取id，challenge，success（是否启用failback）
+                                    url: "{{asset("verify/captcha?rand=")}}" + Math.round(Math.random()*100),
+                                    type: "get",
+                                    dataType: "json",
+                                    success: function (data) {
+                                        // 使用initGeetest接口
+                                        // 参数1：配置参数，与创建Geetest实例时接受的参数一致
+                                        // 参数2：回调，回调的第一个参数验证码对象，之后可以使用它做appendTo之类的事件
+                                        initGeetest({
+                                            gt: data.gt,
+                                            challenge: data.challenge,
+                                            product: "float", // 产品形式
+                                            offline: !data.success
+                                        }, handler);
+                                    }
+                                });
+                            </script>
+                        </div>
                     </div>
-                    <button type="button" class="btn btn-success">下一步</button>
+
+                    <div>
+                        <button type="button" class="btn btn-success">下一步</button>
+                    </div>
                 </form>
             </div>
 
