45d1fd6fe5b969dc469b633e62fbb60b1301bbd3
shuchao <645904877@qq.com>
2016-05-16 15:58:39 +0800

    progress

--- resources/views/user/sign_up.blade.php
+++ resources/views/user/sign_up.blade.php
@@ -9,8 +9,12 @@
             padding: 140px 50px;
         }
 
-        .sign-up-header {
-            padding: 0 200px;
+        .content .sign-up-header {
+            padding: 0 205px;
+        }
+
+        .content .sign-up-header .progress-title {
+            width: 210px;
         }
 
         .content .sign-up-header .fine {
@@ -27,10 +31,24 @@
     <div class="container">
         <div class="content"  ng-app="user">
             <div class="sign-up-header">
+                <div class="progress-title">
+                    <div>
+                        <a>验证手机号</a>
+                    </div>
+                    <div>
+                        <a>填写账号信息</a>
+                    </div>
+                    <div>
+                        <a>注册成功</a>
+                    </div>
+                </div>
                 <div class="progress fine">
                     <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="33.3" aria-valuemin="0" aria-valuemax="100" style="width: 33.3%"></div>
                 </div>
             </div>
+
+
+
             <form ng-controller="sign_up">
                 <div>
                     <input type="text" class="form-control" required ng-model="placeholder.nickname">
