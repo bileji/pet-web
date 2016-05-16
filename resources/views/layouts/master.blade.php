a7ce483399a63eb9a3273e8022a6aa58f06ba8c2
shuchao <645904877@qq.com>
2016-05-16 14:17:31 +0800

    type

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,8 +2,8 @@
     <head>
         <title>@yield('title')</title>
         <script src="{{asset('node_modules/angular/angular.min.js')}}"></script>
-        <link rel="stylesheet" href="{{asset('assets/styles/common/master.css')}}">
         <link rel="stylesheet" href="{{asset('node_modules/bootstrap/dist/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/styles/common/master.css')}}">
         @yield('modules')
     </head>
 
