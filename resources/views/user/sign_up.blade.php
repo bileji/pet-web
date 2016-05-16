3af90536b2c9834f431f282421a8ded52c027275
shuchao <645904877@qq.com>
2016-05-16 15:42:10 +0800

    progress

--- resources/views/user/sign_up.blade.php
+++ resources/views/user/sign_up.blade.php
@@ -6,7 +6,7 @@
     <script src="{{asset('assets/scripts/modules/user/controller.js')}}"></script>
     <style type="text/css">
         .content {
-            padding: 140px 300px;
+            padding: 140px 50px;
         }
 
         .container form div {
@@ -19,7 +19,11 @@
     <div class="container">
         <div class="content"  ng-app="user">
             <div>
-                加入比乐集
+                <div class="progress">
+                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="33.3" aria-valuemin="0" aria-valuemax="100" style="width: 33.3%">
+                        <span class="sr-only">40% Complete (success)</span>
+                    </div>
+                </div>
             </div>
             <form ng-controller="sign_up">
                 <div>
@@ -30,11 +34,11 @@
                     <input type="text" class="form-control" required ng-model="placeholder.phone">
                 </div>
 
-                <div>
-                    <div>
+                <div class="verify">
+                    <div class="">
                         <input type="text" class="form-control" required ng-model="placeholder.verify">
                     </div>
-                    <div>
+                    <div class="image">
                         图形验证码
                     </div>
                 </div>
