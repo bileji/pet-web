3300dac91f6585b6a7224fc5a85c429cbf08a531
shuchao <645904877@qq.com>
2016-05-16 18:14:01 +0800

    progress

--- resources/views/user/sign_up.blade.php
+++ resources/views/user/sign_up.blade.php
@@ -63,9 +63,7 @@
 
                     <script src="http://code.jquery.com/jquery-1.12.3.min.js"></script>
                     <script src="http://static.geetest.com/static/tools/gt.js"></script>
-                    <div id="embed-captcha"></div>
-                    <p id="wait" class="show">正在加载验证码......</p>
-                    <p id="notice" class="hide">请先拖动验证码到相应位置</p>
+                    <div class="gt_slider"><div class="gt_guide_tip gt_show">按住左边滑块，拖动完成上方拼图</div><div class="gt_slider_knob gt_show" style="transform: translate(0px, 0px); -webkit-transform: translate(0px, 0px);"></div><div class="gt_curtain_tip gt_hide">点击上图按钮并沿道路拖动到终点处</div><div class="gt_curtain_knob gt_hide">移动到此开始验证</div><div class="gt_ajax_tip ready"></div></div>
                     <button type="button" class="btn btn-success">下一步</button>
                 </form>
             </div>
