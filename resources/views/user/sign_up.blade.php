31ecfe84144d299cd62f4542f0f7e72662f0571c
shuchao <645904877@qq.com>
2016-05-17 14:11:20 +0800

    min width

--- resources/views/user/sign_up.blade.php
+++ resources/views/user/sign_up.blade.php
@@ -13,7 +13,7 @@
 
         .nav-banner {
             height: 32px;
-            width: 740px;
+            width: 66%;
             margin: auto;
             background-color: #e8e8e8;
         }
@@ -47,7 +47,7 @@
         }
 
         .content .sign-up-header {
-            width: 80%;
+            width: 66%;
             margin: 0 auto;
         }
 
