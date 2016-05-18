86432416884e0be361f9b0da1015d19851d36cc4
shuchao <645904877@qq.com>
2016-05-18 15:53:09 +0800

    fix bug

--- resources/views/user/sign_up.blade.php
+++ resources/views/user/sign_up.blade.php
@@ -226,10 +226,10 @@
 
                 <form name="step2" id="step2" class="relative hide">
                     <div class="table-space">
-                        <input type="text" class="form-control" placeholder="您的昵称" name="nickname" nickname="nickname" ng-model="user.nickname" name-wrong="@{{ step1.ID.$error.nickname || step1.ID.$error.required }}" required>
+                        <input type="text" class="form-control" placeholder="您的昵称" name="nickname" nickname="nickname" ng-model="user.nickname" name-wrong="@{{ step2.nickname.$error.nickname || step2.nickname.$error.required }}" required>
                     </div>
                     <div class="alter">
-                        <span ng-show="!step1.ID.$error.required && step1.ID.$error.nickname" class="hide">
+                        <span ng-show="!step2.nickname.$error.required && step2.nickname.$error.nickname" class="hide">
                             <span class="glyphicon glyphicon-remove x"></span> 昵称格式错误
                         </span>
                     </div>
