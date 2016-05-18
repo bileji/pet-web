60f415ac2d6ea652f88f356fc9f9b54412080ae2
shuchao <645904877@qq.com>
2016-05-18 16:56:47 +0800

    fix bug

--- public/assets/scripts/modules/user/controller.js
+++ public/assets/scripts/modules/user/controller.js
@@ -4,7 +4,7 @@ app.controller('sign_up', ['$scope', function ($scope) {
     $scope.user = {};
 
     $scope.validate = {
-        ID : {
+        ID: {
             format: ''
         }
     }
@@ -33,20 +33,40 @@ app.controller('sign_up', ['$scope', function ($scope) {
 }).directive('nickname', function () {
     var progress_bar = $("#progress-bar");
 
+
     var nickname1 = /^\w+$/;
     var nickname2 = /[\u4e00-\u9fa5]/gm;
+
+    var clear_nickname = function (attrs) {
+        attrs.cache_nickname = "";
+    };
+
+    var save_nickname = function (attrs, nickname) {
+        attrs.cache_nickname = nickname;
+    };
+
+    var progress_bar_plus = function (percent) {
+        var total_width = progress_bar.parent().css("width");
+        progress_bar.css({"width": progress_bar.css("width") + total_width * percent / 100});
+    };
+
+    var progress_bar_reduce = function (percent) {
+        var total_width = progress_bar.parent().css("width");
+        progress_bar.css({"width": progress_bar.css("width") - total_width * percent / 100});
+    };
 
     return {
         restrict: "A",
         require: "ngModel",
         link: function (scope, element, attrs, ngModelController) {
             ngModelController.$parsers.push(function (viewValue) {
-                console.log(attrs);
                 if ((nickname1.test(viewValue) || nickname2.test(viewValue)) && (viewValue.length >= 4 && viewValue.length <= 16)) {
-                    progress_bar.css({"width": "40%"});
+                    progress_bar_plus(10);
+                    save_nickname(attrs, viewValue);
                     ngModelController.$setValidity('nickname', true);
                 } else {
-                    progress_bar.css({"width": "31%"});
+                    !attrs.cache_nickname && progress_bar_reduce(10);
+                    clear_nickname(attrs);
                     ngModelController.$setValidity('nickname', false);
                 }
                 return viewValue;
