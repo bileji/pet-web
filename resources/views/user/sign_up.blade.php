a829010f64e3c3af48b880e254826d18feb476cc
shuchao <645904877@qq.com>
2016-05-22 02:54:01 +0800

    fix bug

--- resources/views/user/sign_up.blade.php
+++ resources/views/user/sign_up.blade.php
@@ -316,7 +316,7 @@
 
             // 验证成功
             captchaObj.onSuccess(function () {
-                console.log(ID.attr("wrong"));
+                console.log(typeof ID.attr("wrong"));
                 console.log(ID.attr("wrong") != "true" || ID.attr("wrong") != true);
                 if (ID.attr("wrong") != "true" || ID.attr("wrong") != true) {
                     check_phone.html(check_phone_html);
