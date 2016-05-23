d985aed3312c0fdd6a25a8c34bddc0e929819b09
shuchao <645904877@qq.com>
2016-05-23 16:34:18 +0800

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,18 +12,16 @@ use App\Http\Responses\Status;
 use App\Http\Services\UserService;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Input;
-use Illuminate\Support\Facades\Request;
 
 class UserController extends Controller
 {
     public function signUp()
     {
+        return Cache::get(Input::get("captcha_token"));
         if (Cache::has(Input::get("captcha_token")) && Cache::get(Input::get("captcha_token")) == Input::get("phone")) {
             return UserService::signUp(Input::all());
         } else {
-            return Response::out(Status::SIGN_UP_INFO_ILLEGALITY, Input::all());
+            return Response::out(Status::SIGN_UP_INFO_ILLEGALITY);
         }
-//        return Response::out(Status::SUCCESS, Request::all());
-//        return UserService::signUp(Request::all());
     }
 }
