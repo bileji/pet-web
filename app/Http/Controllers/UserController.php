262bcbb30c4b18618c9af0b38d8b841301fde276
shuchao <645904877@qq.com>
2016-05-23 16:31:43 +0800

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,13 +10,20 @@ namespace App\Http\Controllers;
 use App\Http\Responses\Response;
 use App\Http\Responses\Status;
 use App\Http\Services\UserService;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Request;
 
 class UserController extends Controller
 {
     public function signUp()
     {
-        return Response::out(Status::SUCCESS, Request::all());
+        if (Cache::has(Input::get("captcha_token")) && Cache::get(Input::get("captcha_token")) == Input::get("phone")) {
+            return UserService::signUp(Input::all());
+        } else {
+            return Response::out(Status::SIGN_UP_INFO_ILLEGALITY, Input::all());
+        }
+//        return Response::out(Status::SUCCESS, Request::all());
 //        return UserService::signUp(Request::all());
     }
 }
--- app/Http/Responses/Status.php
+++ app/Http/Responses/Status.php
@@ -15,16 +15,21 @@ class Status
     const PARAM_ERROR = 1000;
     const BUSINESS_ERROR = 2000;
 
-    const GET_GEE_CAPTCHA_ERROR = 30000;
+    const GET_GEE_CAPTCHA_ERROR = -30000;
+
+    const SIGN_UP_INFO_ILLEGALITY = -40000;
 
     public static $errorMessage = [
         # 系统相关
-        self::FAILED                => '系统错误',
-        self::SUCCESS               => '执行成功',
-        self::PARAM_ERROR           => '参数错误',
-        self::BUSINESS_ERROR        => '业务错误',
+        self::FAILED                  => '系统错误',
+        self::SUCCESS                 => '执行成功',
+        self::PARAM_ERROR             => '参数错误',
+        self::BUSINESS_ERROR          => '业务错误',
 
         # 极验验证码相关
-        self::GET_GEE_CAPTCHA_ERROR => '获取图形验证码失败',
+        self::GET_GEE_CAPTCHA_ERROR   => '获取图形验证码失败',
+
+        # 注册相关
+        self::SIGN_UP_INFO_ILLEGALITY => '非法的注册信息',
     ];
 }
