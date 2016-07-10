18167f6cb59a03fb5078e74fea9cf0fd5d31634f
shuchao <645904877@qq.com>
2016-07-11 00:35:16 +0800

    π

--- resources/views/user/sign_up.blade.php
+++ resources/views/user/sign_up.blade.php
@@ -319,9 +319,9 @@
                     <div class="protocol">
                         <p>
                             己阅读并同意
-                            <a class="legal-link" target="_blank">服务条款</a>
+                            <a class="legal-link" target="_blank">《比乐集服务条款》</a>
                             与
-                            <a class="legal-link" target="_blank">隐私政策</a>
+                            <a class="legal-link" target="_blank">《隐私政策》</a>
                             。
                         </p>
                     </div>
