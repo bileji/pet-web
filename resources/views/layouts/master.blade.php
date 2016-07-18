ee32c0ab3fb9c819fe35a558efe24f36f8d6aff9
shuchao <645904877@qq.com>
2016-07-18 15:03:13 +0800

    font some

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,7 @@
     <title>@yield('title')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <link rel="stylesheet" href="{{asset('node_modules/bootstrap/dist/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/styles/common/css/font-awesome.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/styles/common/master.css')}}">
     <script src="{{asset('node_modules/angular/angular.min.js')}}"></script>
     <script src="{{asset('node_modules/jquery/dist/jquery.min.js')}}"></script>
