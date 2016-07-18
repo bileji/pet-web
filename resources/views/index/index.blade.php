c0da4c3213b84993aa5e964979dcfa7dbc5c30d9
shuchao <645904877@qq.com>
2016-07-18 15:39:30 +0800

    hover

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -171,6 +171,7 @@
 
         .container .menu-wrapper .menu-child-wrapper li a:hover {
             padding: 0 0 0 16px;
+            color: orange;
         }
 
         .container .menu-wrapper .menu-child-wrapper li:last-child a {
