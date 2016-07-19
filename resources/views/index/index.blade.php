d63ca999efab82b4d5cf9127c24202ebe2bee7c6
shuchao <645904877@qq.com>
2016-07-19 23:47:29 +0800

    debug

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -259,7 +259,23 @@
             });
 
             // 轮播
-            $(".owl-carousel").owlCarousel();
+            $(".owl-carousel").owlCarousel({
+                items     : 1,
+                merge     : true,
+                loop      : true,
+                margin    : 10,
+                video     : true,
+                lazyLoad  : true,
+                center    : true,
+                responsive: {
+                    480: {
+                        items: 2
+                    },
+                    600: {
+                        items: 4
+                    }
+                }
+            });
         });
     </script>
 @stop
@@ -448,14 +464,22 @@
         </div>
 
         <div class="content">
-            <div class="owl-carousel">
-                <div> Your Content</div>
-                <div> Your Content</div>
-                <div> Your Content</div>
-                <div> Your Content</div>
-                <div> Your Content</div>
-                <div> Your Content</div>
-                <div> Your Content</div>
+            <div class="owl-carousel owl-theme">
+                <div class="item-video" data-merge="3"><a class="owl-video" href="http://vimeo.com/23924346"></a></div>
+                <div class="item-video" data-merge="1"><a class="owl-video"
+                                                          href="https://www.youtube.com/watch?v=JpxsRwnRwCQ"></a></div>
+                <div class="item-video" data-merge="2"><a class="owl-video"
+                                                          href="http://www.youtube.com/watch?v=FBu_jxT1PkA"></a></div>
+                <div class="item-video" data-merge="1"><a class="owl-video"
+                                                          href="http://www.youtube.com/watch?v=oy18DJwy5lI"></a></div>
+                <div class="item-video" data-merge="2"><a class="owl-video"
+                                                          href="http://www.youtube.com/watch?v=H3jLkJrhHKQ"></a></div>
+                <div class="item-video" data-merge="3"><a class="owl-video"
+                                                          href="https://www.youtube.com/watch?v=g3J4VxWIM6s"></a></div>
+                <div class="item-video" data-merge="1"><a class="owl-video"
+                                                          href="http://www.youtube.com/watch?v=0fhoIate4qI"></a></div>
+                <div class="item-video" data-merge="2"><a class="owl-video"
+                                                          href="https://www.youtube.com/watch?v=EF_kj2ojZaE"></a></div>
             </div>
         </div>
     </div>
