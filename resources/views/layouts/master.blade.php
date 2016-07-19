74cdb0a05ec8080725c5efcb10cd0fb2ff9a3b2a
shuchao <645904877@qq.com>
2016-07-19 18:32:21 +0800

    font-awesome

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,10 +3,10 @@
     <title>@yield('title')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <link rel="stylesheet" href="{{asset('node_modules/bootstrap/dist/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/styles/common/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('node_modules/font-awesome/css/font-awesome.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/styles/common/master.css')}}">
-    <script src="{{asset('node_modules/angular/angular.min.js')}}"></script>
     <script src="{{asset('node_modules/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{asset('node_modules/angular/angular.min.js')}}"></script>
     @yield('modules')
 </head>
 
