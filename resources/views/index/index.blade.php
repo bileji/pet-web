c7340a4455f000712fdb1bf45faeb888b94f913b
shuchao <645904877@qq.com>
2016-07-21 00:10:38 +0800

    fix bug

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -239,11 +239,8 @@
 
         .container .content .top-wrapper .carousel-wrapper .owl-theme .owl-controls {
             margin-top: -28px;
+            position: relative;
         }
-
-        /*.container .content .top-wrapper .carousel-wrapper .owl-theme .owl-controls .owl-pagination .active {*/
-        /*color: rebeccapurple;*/
-        /*}*/
 
         .container .content .top-wrapper .carousel-wrapper {
             width: 400px;
