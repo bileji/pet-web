b83106e309086d7f44a083900b2e049f49b42706
shuchao <645904877@qq.com>
2016-07-21 22:39:01 +0800

    modify

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -159,11 +159,9 @@
             -webkit-transition: .3s all;
         }
 
-        /*.container .menu-wrapper .menu-item:hover {*/
-        /*color: #00a1d6;*/
-        /*border-bottom: 2px solid orange;*/
-        /*cursor: pointer;*/
-        /*}*/
+        .container .menu-wrapper .menu-item:hover {
+            cursor: pointer;
+        }
 
         .container .menu-wrapper .menu-item:first-child {
             width: 76px;
